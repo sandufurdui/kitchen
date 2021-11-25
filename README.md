7da5e1967fa3ff9aec5e4de09ff668344e8b5b93
sandufurdui <76735601+sandufurdui@users.noreply.github.com>
2021-11-25 03:26:03 +0200

    updated readme

--- README.md
+++ README.md
@@ -3,12 +3,25 @@ To get started, run the dining.py from https://github.com/sandufurdui/dining
 (if you have installed locally python, run python dining.py)
 then run kitchen from https://github.com/sandufurdui/dining (python kitchen.py)
 
-#Output
-PS D:\Users\sandu\Desktop\Python\PR\dining> python dining.py
-Connection from  ('127.0.0.1', 60953) (kitchen)
 
-PS D:\Users\sandu\Desktop\Python\PR\kitchen> python .\kitchen.py        
-Received: b'dining hall up at localhost:1500'
+Update
+Improved data exchange between kitchen and dining, now each can send dictionaries(aka json)
+Implemented threading.
+
+#Output
+PS D:\Users\sandu\Desktop\Python\PR\dining> py .\dining.py              
+this is the message sent from kitchen
+{"brand": "chevrolet", "model": "camaro", "year": 2001}
+
+Do you want to continue(y/n) :n
+
+PS D:\Users\sandu\Desktop\Python\PR\kitchen> py .\kitchen.py
+socket binded to port 8000
+kitchen is listening
+this message is sent from dining
+{"brand": "Ford", "model": "Mustang", "year": 1964}
+
+closing connection
 
 
 
--- kitchen.py
+++ kitchen.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 import socket
 
@@ -10,3 +11,81 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = s.recv(1024)
 
 print('Received:', repr(data))
+=======
+# import socket programming library
+import socket
+import json
+import pickle
+with open("menu.json", "r") as menu_json:
+    data1 = json.load(menu_json)
+# import thread module
+data2 = pickle.dumps(data1)
+data3 = 5
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
+
+# thread function
+def threaded(c):
+    while True:
+        
+        #here the data is received
+        message_dining = c.recv(1024)
+        #data is displayed
+        print(message_dining.decode("ascii"))
+
+        if not message_dining:
+            print('closing connection')
+            # lock released on exit
+            print_lock.release()
+            break
+        data1 = {
+            "brand": "chevrolet",
+            "model": "Mustang",
+            "year": 1964
+        }
+        message_kitchen = "this is the message sent from kitchen"
+        message1 = message_kitchen + json.dumps(data1)
+        
+        #data sent back to dining
+        c.send(message1.encode("ascii"))
+
+    # connection closed
+    c.close()
+
+
+def Main():
+    host = "localhost"
+
+    # reverse a port on your computer
+    # in our case it is 12345 but it
+    # can be anything
+    port = 8000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket binded to port", port)
+
+    # put the socket into listening mode
+    s.listen(5) #no of connections
+    print("kitchen is listening")
+
+    # a forever loop until client wants to exit
+    while True:
+
+        # establish connection with client
+        c, addr = s.accept()
+        # lock acquired by client
+        print_lock.acquire()
+        # print('Connected to :', addr[0], ':', addr[1])
+
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (c,))
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
+
+
+>>>>>>> Stashed changes
